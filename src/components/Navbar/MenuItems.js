13cf5a0ae2c93cd2fd726b4d64e40596b7952661
Justin Michelotti <jmichelotti@mosaiclearning.com>
2021-01-10 22:42:02 -0500

    adding basic navbar
    removing unecessary item cName

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,14 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#test',
+    },
+    {
+        title: 'About',
+        url: '#',
+    },
+    {
+        title: 'Contact',
+        url: '#',
+    },
+]
